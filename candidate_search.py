525ac3107d53bbdffd31034cde865f94101391e1
DESKTOP-9NDNENB\pablo <peropaquepo@gmail.com>
2018-04-12 20:32:21 +0100

    Subida de archivos a repositorio

---
+++ candidate_search.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+from SIF import SIF
+import numpy as np
+from time import time
+
+
+year = '15'
+n_params = 32
+
+RD = SIF.RunData(year=year,n_params=n_params)
+#RD = SIF.RunData(year=year,only_HiTS_SN=only_HiTS_SN,n_params=n_params,filter_type='MCC')
+if RD.n_params > 0:
+    RD.apply_params()
+
+init = time()
+
+FH =  SIF.FITSHandler(RD)
+MJD = FH.MJD
+
+KF,SN = RD.deploy_filter_and_detector(MJD)
+
+if RD.SN_index>=0:
+    OB = SIF.Observer(len(MJD),new_pos=RD.SN_pos)
+
+# First run: collect candidates
+
+"""for k in FH.data_names.keys():
+    print k
+    print np.array(FH.data_names[k])
+    print ''"""
+
+
+for o in range(len(MJD)):
+    print 'Beginning with observation: ' + str(o+1).zfill(2) + '/' + str(len(MJD))
+    
+    ti = time()
+    FH.load_fluxes(o)
+    print 'Load and photometry time: ' + str(time()-ti)
+    
+    ti = time()
+    KF.update(MJD[o],FH)
+    print 'Update filter time: ' + str(time()-ti)
+    
+    ti = time()
+    SN.draw_complying_pixel_groups(o,FH,KF)
+    print 'Draw groups time: ' + str(time()-ti)
+    
+    SN.update_candidates(o)
+    
+    if RD.SN_index>=0:
+        OB.rescue_run_data(o,FH,KF,SN)
+    
+    print ''
+    
+# Classify new candidates
+SN.check_candidates(RD)
+
+print 'SN ' + ['not found ','found '][RD.SN_found]
+print 'Number of unidentified objects: ' + str(RD.NUO)
+
+# Evaluate worth of second run
+RD.decide_second_run(OB)
+    
+# Second run: rescue historic info from candidates, if any
+KF,SN = RD.deploy_filter_and_detector(MJD)
+
+OB = SIF.Observer(len(MJD))
+OB.new_objects_from_CandData(RD.CandData)
+
+for o in range(len(MJD)):
+    print 'Beginning with observation: ' + str(o+1).zfill(2) + '/' + str(len(MJD))
+    
+    ti = time()
+    FH.load_fluxes(o)
+    print 'Load and photometry time: ' + str(time()-ti)
+    
+    ti = time()
+    KF.update(MJD[o],FH)
+    print 'Update filter time: ' + str(time()-ti)
+    
+    ti = time()
+    SN.draw_complying_pixel_groups(o,FH,KF)
+    print 'Draw groups time: ' + str(time()-ti)
+    
+    OB.rescue_run_data(o,FH,KF,SN)
+    
+    print ''
+
+RD.save_results(OB)
+
+print 'Total time: '+str(time()-init)
---
+++ candidate_visualization.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from SIF import SIF
+from glob import glob
+import numpy as np
+
+param = 'par-00_'
+HiTS = '*'
+
+#results_dir = 'C:/Users/Phuentelemu/Dropbox/Supernovae/MCKF/results/'
+results_dir = 'C:/cygwin64/home/Phuentelemu/MCKF/results/'
+
+all_results = glob(results_dir + param + 'HiTS' + HiTS + '*.npz')
+
+for result_name in all_results:
+    
+    objects = np.load(result_name)['objects']
+    MJD = objects[0]['MJD']
+    
+    new_obs = SIF.Observer(len(MJD),figsize1=18,figsize2=9.5)
+    new_obs.obj = objects
+    
+    SN_found = result_name.find('AYE') >= 0
+    HiTSSN = result_name[result_name.find('HiTS')+4:result_name.find('HiTS')+6]
+    
+    filename = 'images/' + param + 'HiTS' + HiTSSN
+    
+    new_obs.print_lightcurve(save_filename=filename,SN_found=SN_found)
+    new_obs.print_stamps(save_filename=filename,SN_found=SN_found)
+    
+    for obj in objects:
+        print str(obj['epochs'])+' '+str(obj['posY'])+'-'+str(obj['posX'])+' '+str(obj['status'])

e0e7920d999522ace68a26e8b5b8bca56151d1d6
PalomaPG <paloma.perez.gar@gmail.com>
2018-05-30 16:13:39 -0400

    update bash

--- orig-sif/candidate_search.sh
+++ orig-sif/candidate_search.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name="candidate_search"
 #SBATCH --partition=slims
-#SBATCH -c 4
+#SBATCH -n 1
 #SBATCH --mem-per-cpu=2400
 #SBATCH --output=outputs/CS-%j.out
 #SBATCH --error=outputs/CS-%j.err
+#SBATCH --mail-user=paloma.perez.gar@gmail.com
+#SBATCH --mail-type=ALL
 #SBATCH --array=0-2975
 #SBATCH --exclusive=user
 
-module load Lmod/6.5
-source $LMOD_PROFILE
-ml icc/2017.4.196-GCC-6.4.0-2.28 impi/2017.3.196 Python/3.6.3
-
-echo $SLURM_ARRAY_TASK_ID;
+echo $SLURM_ARRAY_TASK_ID
 echo "SLURM_JOBID="$SLURM_JOBID
 echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 echo "SLURM_NNODES"=$SLURM_NNODES
 echo "SLURMTMPDIR="$SLURMTMPDIR
 echo "working directory="$SLURM_SUBMIT_DIR
 
-python -m cProfile -o profile_sif candidate_search.py
+python -m cProfile -o sif_profile candidate_search.py $SLURM_ARRAY_TASK_ID
 echo "Trabajo terminado";

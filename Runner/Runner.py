2cf2678334f9044f8b360028b9bfc7b01b71060a
PalomaPG <paloma.perez.gar@gmail.com>
2018-06-14 20:21:08 -0400

    New methods in Runner (selecting data according to availability and airmass)

--- Runner/Runner.py
+++ Runner/Runner.py
@@ -3,8 +3,6 @@ import os
 import numpy as np
 from astropy.io import fits
 
-from FluxCalculator import FluxCalculator
-
 
 class Runner(object):
 
@@ -21,19 +19,32 @@ class Runner(object):
 
         self.set_data()
 
-        #self.flux_calc = FluxCalculator()
-
     def set_data(self):
+        self.collect_data()
+        self.select_science_images()
+        self.select_images_dirs()
+
+    def select_images_dirs(self):
+        self.select_images('baseDir')
+        self.select_images('maskDir')
+        self.select_images('crblastDir')
+
+        self.select_images('diffDir')
+        self.select_images('invDir')
+        self.select_images('afluxDir')
+        self.select_images('psfDir')
+
+    def collect_data(self):
 
         self.data['baseDir'] = self.walking_through_files('baseRegEx', 'baseDir')
         self.data['maskDir'] = self.walking_through_files('maskRegEx', 'maskDir')
         self.data['crblastDir'] = self.walking_through_files('crblastRegEx', 'crblastDir')
         self.data['scienceDir'] = self.walking_through_files('scienceRegEx', 'scienceDir')
 
-        self.data['psfDir'] = self.walking_through_files('psfRegEx', 'psfDir')
         self.data['diffDir'] = self.walking_through_files('diffRegEx', 'diffDir')
         self.data['invDir'] = self.walking_through_files('invRegEx', 'invDir')
         self.data['afluxDir'] = self.walking_through_files('afluxRegEx', 'afluxDir')
+        self.data['psfDir'] = self.walking_through_files('psfRegEx', 'psfDir')
 
     def walking_through_files(self, regex, dir_):
 
@@ -47,15 +58,39 @@ class Runner(object):
     def get_files(self, dir_key):
         return self.data[dir_key]
 
-    def get_mjd_lst(self):
-        return [float(fits.open(m)[0].header['MJD-OBS']) for m in self.data['scienceDir']]
-
-    def get_airmass_lst(self):
-        return [float(fits.open(m)[0].header['AIRMASS']) for m in self.data['scienceDir']]
-
-    def select_data(self):
-        pass
-
+    def select_science_images(self):
         """
         listar mjd y airmass, hacer seleccion de acuerdo a estos valores
         """
+        data_science = []
+
+        for fits_image in self.data['scienceDir']:
+            with fits.open(fits_image) as opened_fits_image:
+                if float(opened_fits_image[0].header['AIRMASS']) < 1.7:
+                    data_science.append(fits_image)
+
+        self.data['scienceDir'] = data_science
+    # Append in order
+
+    def select_images(self, dir_):
+        new_dir = []
+
+        for science_image in self.data['scienceDir']:
+
+            science_prefix = (os.path.basename(os.path.normpath(science_image)))
+            science_prefix = science_prefix[:[m.start() for m in re.finditer(r"_", science_prefix)][2]+3]
+            print(fits.open(science_image)[0].header['MJD-OBS'], science_prefix)
+            isin = False
+            for fits_file in self.data[dir_]:
+                if fits_file.find(science_prefix) > -1:
+                    new_dir.append(fits_file)
+                    isin = True
+                    break
+            if not isin:
+                #print(science_image)
+                self.data['scienceDir'].remove(science_image)
+
+        print("Science image number: %d, number of resultant images in directory %s: %d"
+             % (len(self.data['scienceDir']), dir_, len(new_dir)))
+
+        self.data[dir_] = new_dir
--- Runner/runner_main.py
+++ Runner/runner_main.py
@@ -5,7 +5,11 @@ import sys
 def main():
 
     runner = Runner(sys.argv[1])
-    print(runner.get_mjd_lst())
+    #print('Lista de archivos PSF')
+    print(len(runner.get_files('baseDir')))
+    print(len(runner.get_files('maskDir')))
+    print(len(runner.get_files('crblastDir')))
+    print(len(runner.get_files('scienceDir')))
 
 
 if __name__ == '__main__':

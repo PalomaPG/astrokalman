634f7345002022c076c723ee461557cc0eea5ba0
PalomaPG <paloma.perez.gar@gmail.com>
2018-07-23 10:28:52 -0400

    candidate_visualization.py

--- orig-sif/candidate_visualization.py
+++ orig-sif/candidate_visualization.py
@@ -7,7 +7,7 @@ param = 'par-00_'
 HiTS = '*'
 
 #results_dir = 'C:/Users/Phuentelemu/Dropbox/Supernovae/MCKF/results/'
-results_dir = '/home/paloma/Documents/Memoria/Code/sif2/orig-sif/results/'
+results_dir = '/home/pperez/Thesis/results/'
 
 #all_results = glob(results_dir + param + 'HiTS' + HiTS + '*.npz')
 all_results = glob(results_dir + '*.npz')
@@ -21,7 +21,7 @@ for result_name in all_results:
     
     SN_found = result_name.find('AYE') >= 0
     HiTSSN = result_name[result_name.find('HiTS')+4:result_name.find('HiTS')+6]
-    filename = '/home/paloma/Documents/Memoria/Code/sif2/orig-sif/images/' + param + 'HiTS' + HiTSSN
+    filename = '/home/pperez/Thesis/images/' + param + 'HiTS' + HiTSSN
 
     new_obs.print_lightcurve(MJD=MJD, obj=new_obs.obj, save_filename=filename, SN_found=SN_found)
     new_obs.print_stamps(MJD=MJD, obj=new_obs.obj, save_filename=filename, SN_found=SN_found)

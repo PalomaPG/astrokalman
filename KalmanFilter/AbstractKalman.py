d1e46ec08c43f8fb31cd1c794393af33b53a2073
PalomaPG <paloma.perez.gar@gmail.com>
2018-09-05 16:35:23 -0300

    Kalman filter

--- KalmanFilter/AbstractKalman.py
+++ KalmanFilter/AbstractKalman.py
@@ -1,13 +1,24 @@
+import numpy as np
+
 from abc import ABC, abstractclassmethod
 
 class AbstractKalman(ABC):
+
+    def __init__(self, image_size):
+        self.image_size = image_size
+        self.state = np.zeros((2, ) + self.image_size, dtype=float)
+        self.state_cov = np.zeros((3, ) + image_size, dtype=float)
+
+    def initialize_states(self, init_state):
+        self.state[0] = init_state[0]
+        self.state[1] = init_state[1]
 
     @abstractclassmethod
     def predict(self):
         pass
 
     @abstractclassmethod
-    def correct(self):
+    def correct(self,  pred_state, pred_cov):
         pass
 
     def update(self):
---
+++ tests/general_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+import numpy as np
+from KalmanFilter.LinearKalman import LinearKalman
+
+class TestKalmanLinearFilters(unittest.TestCase):
+
+
+
+
+    def test_buildBasicFilter(self):
+        image_size = (4094, 2046)
+        flux = np.zeros(image_size)
+        vflux = np.zeros(image_size)
+        init_state = [flux, vflux]
+        kalman = LinearKalman()
+        self.assertTrue(kalman.predict(10.5, 12.0) == 1.5)
+
+    def test_states(self):
+        image_size = (4094, 2046)
+        flux = np.ones(image_size)*7.3
+        vflux = np.ones(image_size)*5
+        init_state = [flux, vflux]
+        kalman = LinearKalman()
+        kalman.initialize_states(init_state=init_state)
+        kalman.predict(5.6, 10.7)
+
+if __name__ == '__main__':
+    unittest.main()

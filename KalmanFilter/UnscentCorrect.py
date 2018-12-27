f8b72beb1a1c9e77d861af0d41f5e445c6bf4107
PalomaPG <paloma.perez.gar@gmail.com>
2018-12-27 15:40:45 -0300

    little changes.. and a fix to sn lst

--- KalmanFilter/UnscentCorrect.py
+++ KalmanFilter/UnscentCorrect.py
@@ -26,8 +26,8 @@ class UnscentCorrect(ICorrect):
 
     def correct(self, z, R, pred_state, pred_cov, state, state_cov):
         print(self.delta_t)
-        #Ys = sigma_points(pred_state, pred_cov, lambda_=self.lambda_, d=self.d)
-        state, state_cov = propagate_func(self.f_func, self.Wm, self.Wc, self.Xs, self.delta_t) # z^, S_k - R
+        Ys = sigma_points(pred_state, pred_cov, lambda_=self.lambda_, d=self.d)
+        state, state_cov = propagate_func(self.f_func, self.Wm, self.Wc, Ys, self.delta_t) # z^, S_k - R
         #Residual
         residual = z - state
         S = state_cov + R
@@ -39,8 +39,8 @@ class UnscentCorrect(ICorrect):
         D = 2*self.d+1
 
         for i in range(D):
-            h_diff.append(perform(self.h_func, self.Xs[i],))
-            f_diff.append(perform(self.f_func, self.Xs[i], (self.delta_t,)))
+            h_diff.append(perform(self.h_func, Ys[i],))
+            f_diff.append(perform(self.f_func, Ys[i], (self.delta_t,)))
 
         for i in range(D):
             h_diff[i] = h_diff[i] - state
@@ -52,43 +52,4 @@ class UnscentCorrect(ICorrect):
         state = pred_state + matrix_vector_dot_product(K, residual)
         state_cov = pred_cov - matrices_dot_product(K, matrices_dot_product(S, K))#K @ S @ K.T
         return state, state_cov
-
-
-def multiple_dot_products(m1_lst, m2_lst, w_c, image_size):
-    product = np.zeros(shape=tuple([3]) + image_size)
-    l = len(m1_lst)
-    for i in range(l):
-        product[0] = w_c[i]*m1_lst[i][0, :]*m2_lst[i][0, :]+product[0]
-        product[1] = w_c[i]*m1_lst[i][0, :]*m2_lst[i][1, :]+product[1]
-        product[2] = w_c[i]*m1_lst[i][1, :]*m2_lst[i][1, :]+product[2]
-
-    return product
-
-
-def matrix_inverse(mat):
-    new_mat = np.zeros(shape=mat.shape)
-    factor = mat[0] * mat[2] - mat[1] * mat[1]
-    new_mat[0] = mat[2]/factor
-    new_mat[1] = -mat[1]/factor
-    new_mat[2] = mat[0]/factor
-    return new_mat
-
-
-def matrices_dot_product(m1, m2):
-
-    product = np.zeros(shape=m1.shape)
-    product[0] = m1[0]*m2[0] + m1[1]*m2[1]
-    product[1] = m1[0]*m2[1] + m1[1]*m2[2]
-    product[2] = m1[1]*m2[1] + m1[2]*m2[2]
-    return product
-
-
-def matrix_vector_dot_product(m, v):
-    product = np.zeros(shape=v.shape)
-    product[0] = m[0]*v[0] + m[1]*v[1]
-    product[1] = m[1]*v[0] + m[2]*v[1]
-    return product
-
-
-
 
--- modules/unscented_utils.py
+++ modules/unscented_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 ########  ########
 
 
-def sigma_points(mean_, cov_, lambda_,  d=2):
+def sigma_points(mean_, cov_, lambda_,  N=2):
     """
 
     :param mean_:
@@ -15,7 +15,7 @@ def sigma_points(mean_, cov_, lambda_,  d=2):
     :param D:
     :return:
     """
-    L, U = cholesky((d+lambda_)*cov_)
+    L, U = cholesky((N+lambda_)*cov_)
     X_0 = mean_
     X_1 = mean_ + L[[0, 1], :]
     X_2 = mean_ + L[[1, 2], :]
@@ -25,14 +25,14 @@ def sigma_points(mean_, cov_, lambda_,  d=2):
     return list([X_0, X_1, X_2, X_3, X_4])
 
 
-def unscent_weights(kappa=0, alpha=10**(-3), beta=2.0, D=2):
-    lambda_ = (alpha ** 2) * (D + kappa) - D
-    W_m = np.zeros(2*D+1)
-    W_m[0] = lambda_ / (D + lambda_)
-    W_m[1:] = 1/(2*(D + lambda_))
-    W_c = np.zeros(2*D+1)
+def unscent_weights(kappa=0, alpha=10**(-3), beta=2.0, N=2):
+    lambda_ = (alpha ** 2) * (N + kappa) - N
+    W_m = np.zeros(2*N+1)
+    W_m[0] = lambda_ / (N + lambda_)
+    W_m[1:] = 1/(2*(N + lambda_))
+    W_c = np.zeros(2*N+1)
     W_c[0] = W_m[0] + (1 - alpha**2 + beta)
-    W_c[1:] = 1/(2*(D + lambda_))
+    W_c[1:] = 1/(2*(N + lambda_))
     return W_m, W_c
 
 def perform(func, *args):
@@ -59,6 +59,46 @@ def propagate_func(func, W_m, W_c,  Xs, *args, D=2 ):
         y_cov[1] += W_c[i] * (y_diff_0*y_diff_2)
 
     return y_mean, y_cov
+
+
+
+def multiple_dot_products(m1_lst, m2_lst, w_c, image_size):
+    product = np.zeros(shape=tuple([3]) + image_size)
+    l = len(m1_lst)
+    for i in range(l):
+        product[0] = w_c[i]*m1_lst[i][0, :]*m2_lst[i][0, :]+product[0]
+        product[1] = w_c[i]*m1_lst[i][0, :]*m2_lst[i][1, :]+product[1]
+        product[2] = w_c[i]*m1_lst[i][1, :]*m2_lst[i][1, :]+product[2]
+
+    return product
+
+
+def matrix_inverse(mat):
+    new_mat = np.zeros(shape=mat.shape)
+    factor = mat[0] * mat[2] - mat[1] * mat[1]
+    new_mat[0] = mat[2]/factor
+    new_mat[1] = -mat[1]/factor
+    new_mat[2] = mat[0]/factor
+    return new_mat
+
+
+def matrices_dot_product(m1, m2):
+
+    product = np.zeros(shape=m1.shape)
+    product[0] = m1[0]*m2[0] + m1[1]*m2[1]
+    product[1] = m1[0]*m2[1] + m1[1]*m2[2]
+    product[2] = m1[1]*m2[1] + m1[2]*m2[2]
+    return product
+
+
+def matrix_vector_dot_product(m, v):
+    product = np.zeros(shape=v.shape)
+    product[0] = m[0]*v[0] + m[1]*v[1]
+    product[1] = m[1]*v[0] + m[2]*v[1]
+    return product
+
+
+
 
 
 ######## LINEAR FUNCTIONS #########
